732302f8468611a0442d3d279475081ea82b6c62
Vishesh R Shetty <64135749+visheshshetty86@users.noreply.github.com>
2022-08-24 12:05:29 +0530

    Add files via upload

---
+++ lucky.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class lucky {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        num=sc.nextInt();
+
+        if(num%5==0)
+        {
+            System.out.println("lucky");
+        }
+        else
+        {
+            System.out.println("");
+        }
+        }
+        }
