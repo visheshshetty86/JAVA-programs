6b371c84450d0a80f92e15c5aa76d19ccdeb91fb
Vishesh R Shetty <64135749+visheshshetty86@users.noreply.github.com>
2022-08-24 10:16:09 +0530

    Add files via upload

---
+++ divisible.java
@@ -0,0 +1,15 @@
+public class divisible {
+    public static void main(String args[])
+    {
+        int x=43;
+        if(x%5==0){
+            System.out.println("yes");
+        }
+        else if(x%3==0){
+                System.out.println("it is divisible by 3" );
+            }
+            else{
+                System.out.println("plese enter number corrctly");
+            }
+    }
+}
